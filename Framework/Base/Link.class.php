634200cb7dbe34a99b731c272d07e1c0690d0f22
Dennis Eichhorn <spl1nes.com@googlemail.com>
2014-08-11 17:51:52 +0200

    Experimental elements.

---
+++ Framework/Base/Link.class.php
@@ -0,0 +1,16 @@
+<?php
+/* WARNING: EXPERIMENTAL */
+namespace Framework\Base {
+    class Link {
+        public $trigger = null;
+        public $data = null;
+        public $string = null;
+        public $classes = null;
+
+        public function __construct() {}
+
+        public function add_data() {}
+
+        public function render() {}
+    }
+}
---
+++ Framework/Base/Table.class.php
@@ -0,0 +1,27 @@
+<?php
+/* WARNING: EXPERIMENTAL */
+namespace Framework\Base {
+    class Table {
+        public function __construct() {}
+
+        public function set_table() {}
+
+        public function set_header($header) {}
+
+        public function set_table_data($data) {}
+
+        public function add_header($header) {}
+
+        public function remove_header($header) {}
+
+        public function add_dataset($header, $data) {}
+
+        public function remove_dataset($header) {}
+
+        public function render_header() {}
+
+        public function render_content() {}
+
+        public function render_pagination() {}
+    }
+}
