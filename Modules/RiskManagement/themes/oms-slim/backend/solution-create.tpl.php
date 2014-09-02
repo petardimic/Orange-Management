bd2a33a6c72621157492d6de18a92608aa98f433
Dennis Eichhorn <spl1nes.com@googlemail.com>
2014-09-02 09:49:35 +0200

    Create solution-create.tpl.php

---
+++ Modules/RiskManagement/themes/oms-slim/backend/solution-create.tpl.php
@@ -0,0 +1,38 @@
+<?php /** @var \Modules\RiskManagement\Handler $this */
+\Framework\Module\ModuleFactory::$initialized[1000500000]->show([\Modules\Navigation\NavigationType::CONTENT, 1003001001]);
+?>
+
+<div class="b b-2 c30-1 c30" id="i30-1-1">
+    <h1>
+        <?= \Framework\Localization\Localization::$lang[30]['Solution']; ?>
+        <i class="fa fa-minus min"></i>
+        <i class="fa fa-plus max vh"></i>
+    </h1>
+
+    <div class="bc-1">
+        <ul class="l-1">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Title']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Description']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Unit']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Protection']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Probability']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Damage']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Cause']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Parent']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Department']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Category']; ?>
+            <li><input type="text">
+            <li><?= \Framework\Localization\Localization::$lang[30]['Active']; ?>
+            <li><input type="text">
+        </ul>
+    </div>
+</div>
