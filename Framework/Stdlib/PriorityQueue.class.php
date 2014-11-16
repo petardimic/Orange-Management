4b1f120a192e774c73d1f47aaa0eff97fedd874c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2014-11-16 14:09:17 +0100

    Sketching

--- Framework/Stdlib/PriorityQueue.class.php
+++ Framework/Stdlib/PriorityQueue.class.php
@@ -1,11 +1,11 @@
 <?php
-namespace Framework\Datatypes {
+namespace Framework\Stdlib {
     /**
      * PriorityQueue class
      *
      * PHP Version 5.4
      *
-     * @category   Datatypes
+     * @category   Stdlib
      * @package    Framework
      * @author     OMS Development Team <dev@oms.com>
      * @author     Dennis Eichhorn <d.eichhorn@oms.com>
@@ -19,20 +19,58 @@ namespace Framework\Datatypes {
         private $count = 0;
         private $queue = [];
 
-        public function insert() {
+        public function insert($data, $priority = 0) {
+            do {
+                $key = rand();
+            } while(array_key_exists($key, $this->queue));
 
+            if($this->count === 0) {
+                $this->queue[$key] = ['key' => $key, 'data' => $data, 'priority' => $priority];
+            } else {
+                $pos = 0;
+                foreach($this->queue as $ele) {
+                    if($ele['priority'] < $priority) {
+                        break;
+                    }
+
+                    $pos++;
+                }
+
+                array_splice($original, $pos, 0, [$key => ['key' => $key, 'data' => $data, 'priority' => $priority]]);
+            }
+
+            return $key;
+        }
+
+        public function increaseAll($increase) {
+            foreach($queue as $key => &$ele) {
+                $ele['priority'] += $increase;
+            }
         }
 
         public function get() {
-
+            $ele = array_pop($this->queue);
+            $this->queue[$ele['key']] = $ele;
         }
 
-        public function delete() {
-
+        public function delete($id = null) {
+            if($id === null) {
+                $this->remove();
+            } else {
+                unser($this->queue[$id]);
+            }
         }
 
         public function remove() {
+            return array_pop($this->queue);
+        }
 
+        public function setPriority($id, $priority) {
+            $this->queue[$id]['priority'] = $priority;
+        }
+
+        public function getPriority($id) {
+            return $this->queue[$id]['priority'];
         }
 
         public function count() {
@@ -40,11 +78,12 @@ namespace Framework\Datatypes {
         }
 
         public function serialize() {
-
+            return json_encode($this->queue);
         }
 
         public function unserialize($data) {
-
+            $this->queue = json_decode($data);
+            $this->count = count($this->queue);
         }
     }
 }
