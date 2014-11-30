ae7a7bf3346d910a476d393fecb8539fb7615d20
Dennis Eichhorn <spl1nes.com@googlemail.com>
2014-11-30 20:21:31 +0100

    Linux setup

---
+++ Docs/setup.sh
@@ -0,0 +1,51 @@
+sudo apt-get update
+
+sudo apt-get install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+
+sudo apt-get purge openjdk*
+
+sudo apt-get install software-properties-common
+
+sudo add-apt-repository ppa:webupd8team/java
+
+sudo apt-get update
+
+sudo apt-get install oracle-java8-installer
+
+wget http://download.jetbrains.com/webide/PhpStorm-8.0.1.tar.gz
+
+tar -zxvf PhpStorm-8.0.1.tar.gz
+
+cd PhpStorm-138.2001.2328/bin
+
+chmod +x phpstorm.sh
+
+sudo apt-get install git
+
+cd
+
+mkdir Dev
+
+sudo apt-get install npm
+
+sudo npm install -g grunt-cli
+
+sudo apt-get install nodejs
+
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+cd Dev/Orange-Management
+
+sudo npm install
+
+sudo apt-get install curl
+
+sudo apt-get install php5 php5-cli php5-xsl
+
+php composer.phar install
+
+sudo apt-get install pdepend
+
+sudo apt-get install phploc
+
+sudo gem install sass
