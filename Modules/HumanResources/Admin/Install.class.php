29df55561ee13295b14e6df28d5782cd5a22f6bf
Dennis Eichhorn <spl1nes.com@googlemail.com>
2014-12-12 20:11:02 +0100

    HR db installation

--- Modules/HumanResources/Admin/Install.class.php
+++ Modules/HumanResources/Admin/Install.class.php
@@ -28,7 +28,53 @@ namespace Modules\HumanResources\Admin {
         public static function install(&$db, $info) {
             switch($db->getType()) {
                 case \Framework\DataStorage\Database\DatabaseType::MYSQL:
+                    $db->con->prepare(
+                        'CREATE TABLE if NOT EXISTS `' . $db->prefix . 'hr_department` (
+                            `HRDepartmentID` int(11) NOT NULL AUTO_INCREMENT,
+                            `name`  varchar(30) NOT NULL,
+                            `parent` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`HRDepartmentID`),
+                            KEY `parent` (`parent`)
+                        )ENGINE=InnoDB  DEFAULT CHARSET=utf8;'
+                    )->execute();
 
+                    $db->con->prepare(
+                        'ALTER TABLE `' . $db->prefix . 'hr_department`
+                            ADD CONSTRAINT `hr_department_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `' . $db->prefix . 'hr_department` (`HRDepartmentID`);'
+                    )->execute();
+
+                    $db->con->prepare(
+                        'CREATE TABLE if NOT EXISTS `' . $db->prefix . 'hr_staff` (
+                            `HRStaffID` int(11) NOT NULL AUTO_INCREMENT,
+                            `status` tinyint(2) DEFAULT NULL,
+                            `person` int(11) DEFAULT NULL,
+                            PRIMARY KEY (`HRStaffID`),
+                            KEY `parent` (`parent`)
+                        )ENGINE=InnoDB  DEFAULT CHARSET=utf8;'
+                    )->execute();
+
+                    $db->con->prepare(
+                        'ALTER TABLE `' . $db->prefix . 'hr_staff`
+                            ADD CONSTRAINT `hr_staff_ibfk_1` FOREIGN KEY (`person`) REFERENCES `' . $db->prefix . 'accounts` (`id`);'
+                    )->execute();
+
+                    $db->con->prepare(
+                        'CREATE TABLE if NOT EXISTS `' . $db->prefix . 'hr_staff_history` (
+                            `HRStaffHistID` int(11) NOT NULL AUTO_INCREMENT,
+                            `staff` int(11) DEFAULT NULL,
+                            `position` int(11) DEFAULT NULL,
+                            `department` int(11) DEFAULT NULL,
+                            `start` datetime DEFAULT NULL,
+                            `end` datetime DEFAULT NULL,
+                            PRIMARY KEY (`HRStaffHistID`),
+                            KEY `staff` (`staff`)
+                        )ENGINE=InnoDB  DEFAULT CHARSET=utf8;'
+                    )->execute();
+
+                    $db->con->prepare(
+                        'ALTER TABLE `' . $db->prefix . 'hr_staff_history`
+                            ADD CONSTRAINT `hr_staff_history_ibfk_1` FOREIGN KEY (`staff`) REFERENCES `' . $db->prefix . 'hr_staff` (`HRStaffID`);'
+                    )->execute();
                     break;
             }
 
