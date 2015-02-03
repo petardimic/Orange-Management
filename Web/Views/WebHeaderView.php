8259c4c03758d823adb0fce76ed5f81466f352d5
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-02-03 14:47:27 +0100

    init

--- Web/Views/WebHeaderView.php
+++ Web/Views/WebHeaderView.php
@@ -1,1 +1,14 @@
 <?php
+namespace Web\Views;
+
+class WebHeaderView extends \Framework\Views\ViewAbstract {
+  protected $charset = 'utf-8';
+  
+  protected $title = '';
+  
+  protected $keywords = [];
+  
+  protected $author = null;
+  
+  protected $language = null;
+}
