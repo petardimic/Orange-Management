a44e92cfb5f90a83421d9b9e3951a6c2124aad36
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-03-28 10:22:28 +0100

    Creating dedicated l11n classes

---
+++ phpOMS/Localization/ISO4217Enum.php
@@ -0,0 +1,135 @@
+<?php
+namespace phpOMS\Localization;
+
+/**
+ * Country codes ISO list
+ *
+ * PHP Version 5.4
+ *
+ * @category   Framework
+ * @package    phpOMS\DataStorage\Database
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+abstract class ISO4217Enum extends \phpOMS\Datatypes\Enum
+{
+    const ALL = "Albania, Leke";
+    const AFN = "Afghanistan, Afghanis";
+    const ARS = "Argentina, Pesos";
+    const AWG = "Aruba, Guilders";
+    const AUD = "Australia, Dollars";
+    const AZN = "Azerbaijan, New Manats";
+    const BSD = "Bahamas, Dollars";
+    const BBD = "Barbados, Dollars";
+    const BYR = "Belarus, Rubles";
+    const BZD = "Belize, Dollars";
+    const BMD = "Bermuda, Dollars";
+    const BOB = "Bolivia, Bolivianos";
+    const BAM = "Bosnia and Herzegovina, Convertible Marka";
+    const BWP = "Botswana, Pulas";
+    const BGN = "Bulgaria, Leva";
+    const BRL = "Brazil, Reais";
+    const BND = "Brunei Darussalam, Dollars";
+    const KHR = "Cambodia, Riels";
+    const CAD = "Canada, Dollars";
+    const KYD = "Cayman Islands, Dollars";
+    const CLP = "Chile, Pesos";
+    const CNY = "China, Yuan Renminbi";
+    const COP = "Colombia, Pesos";
+    const CRC = "Costa Rica, Colón";
+    const HRK = "Croatia, Kuna";
+    const CUP = "Cuba, Pesos";
+    const CZK = "Czech Republic, Koruny";
+    const DKK = "Denmark, Kroner";
+    const DOP = "Dominican Republic, Pesos";
+    const XCD = "East Caribbean, Dollars";
+    const EGP = "Egypt, Pounds";
+    const SVC = "El Salvador, Colones";
+    const EEK = "Estonia, Krooni";
+    const EUR = "Euro";
+    const FKP = "Falkland Islands, Pounds";
+    const FJD = "Fiji, Dollars";
+    const GHC = "Ghana, Cedis";
+    const GIP = "Gibraltar, Pounds";
+    const GTQ = "Guatemala, Quetzales";
+    const GGP = "Guernsey, Pounds";
+    const GYD = "Guyana, Dollars";
+    const HNL = "Honduras, Lempiras";
+    const HKD = "Hong Kong, Dollars";
+    const HUF = "Hungary, Forint";
+    const ISK = "Iceland, Kronur";
+    const INR = "India, Rupees";
+    const IDR = "Indonesia, Rupiahs";
+    const IRR = "Iran, Rials";
+    const IMP = "Isle of Man, Pounds";
+    const ILS = "Israel, New Shekels";
+    const JMD = "Jamaica, Dollars";
+    const JPY = "Japan, Yen";
+    const JEP = "Jersey, Pounds";
+    const KZT = "Kazakhstan, Tenge";
+    const KES = "Kenyan Shilling";
+    const KGS = "Kyrgyzstan, Soms";
+    const LAK = "Laos, Kips";
+    const LVL = "Latvia, Lati";
+    const LBP = "Lebanon, Pounds";
+    const LRD = "Liberia, Dollars";
+    const LTL = "Lithuania, Litai";
+    const MKD = "Macedonia, Denars";
+    const MYR = "Malaysia, Ringgits";
+    const MUR = "Mauritius, Rupees";
+    const MXN = "Mexico, Pesos";
+    const MNT = "Mongolia, Tugriks";
+    const MZN = "Mozambique, Meticais";
+    const NAD = "Namibia, Dollars";
+    const NPR = "Nepal, Rupees";
+    const ANG = "Netherlands Antilles, Guilders";
+    const NZD = "New Zealand, Dollars";
+    const NIO = "Nicaragua, Cordobas";
+    const NGN = "Nigeria, Nairas";
+    const KPW = "North Korea, Won";
+    const NOK = "Norway, Krone";
+    const OMR = "Oman, Rials";
+    const PKR = "Pakistan, Rupees";
+    const PAB = "Panama, Balboa";
+    const PYG = "Paraguay, Guarani";
+    const PEN = "Peru, Nuevos Soles";
+    const PHP = "Philippines, Pesos";
+    const PLN = "Poland, Zlotych";
+    const QAR = "Qatar, Rials";
+    const RON = "Romania, New Lei";
+    const RUB = "Russia, Rubles";
+    const SHP = "Saint Helena, Pounds";
+    const SAR = "Saudi Arabia, Riyals";
+    const RSD = "Serbia, Dinars";
+    const SCR = "Seychelles, Rupees";
+    const SGD = "Singapore, Dollars";
+    const SBD = "Solomon Islands, Dollars";
+    const SOS = "Somalia, Shillings";
+    const ZAR = "South Africa, Rand";
+    const KRW = "South Korea, Won";
+    const LKR = "Sri Lanka, Rupees";
+    const SEK = "Sweden, Kronor";
+    const CHF = "Switzerland, Francs";
+    const SRD = "Suriname, Dollars";
+    const SYP = "Syria, Pounds";
+    const TWD = "Taiwan, New Dollars";
+    const THB = "Thailand, Baht";
+    const TTD = "Trinidad and Tobago, Dollars";
+    const TRY = "Turkey, Lira";
+    const TRL = "Turkey, Liras";
+    const TVD = "Tuvalu, Dollars";
+    const UAH = "Ukraine, Hryvnia";
+    const GBP = "United Kingdom, Pounds";
+    const USD = "United States of America, Dollars";
+    const UYU = "Uruguay, Pesos";
+    const UZS = "Uzbekistan, Sums";
+    const VEF = "Venezuela, Bolivares Fuertes";
+    const VND = "Vietnam, Dong";
+    const YER = "Yemen, Rials";
+    const ZWD = "Zimbabwe, Zimbabwe Dollars";
+}
