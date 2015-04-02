479cfa97b7691a37e7313b71b6d5d184b50f4287
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-04-02 19:46:50 +0200

    Factory+Mysql connection

---
+++ phpOMS/DataStorage/Database/Connection/ConnectionFactory.php
@@ -0,0 +1,48 @@
+<?php
+namespace phpOMS\DataStorage\Database\Connection;
+
+/**
+ * Database connection factory
+ *
+ * PHP Version 5.4
+ *
+ * @category   Framework
+ * @package    phpOMS\DataStorage\Database
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class ConnectionFactory
+{
+    /**
+     * Constructor
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    private function __construct() {}
+
+    /**
+     * Connect to database
+     *
+     * Overwrites current connection if existing
+     *
+     * @param string[] $dbdata the basic database information for establishing a connection
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public static function create($dbdata) {
+        switch($dbdata['db']) {
+            case 'mysql':
+                return new \phpOMS\DataStorage\Database\Connection\MysqlConnection($dbdata);
+                break;
+            default:
+                throw new \Exception('Unknown database type');
+        }
+    }
+}
---
+++ phpOMS/DataStorage/Database/Connection/MysqlConnection.php
@@ -0,0 +1,149 @@
+<?php
+namespace phpOMS\DataStorage\Database\Connection;
+
+/**
+ * Database handler
+ *
+ * Handles the database connection.
+ * Implementing wrapper functions for multiple databases is planned (far away).
+ *
+ * PHP Version 5.4
+ *
+ * @category   Framework
+ * @package    phpOMS\DataStorage\Database
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class MysqlConnection
+{
+    /**
+     * Connection object
+     *
+     * This can be used externally to define queries and execute them.
+     *
+     * @var \PDO
+     * @since 1.0.0
+     */
+    public $con = null;
+
+    /**
+     * Database data
+     *
+     * @var string[]
+     * @since 1.0.0
+     */
+    public $dbdata = null;
+
+    /**
+     * Database prefix
+     *
+     * The database prefix name for unique table names
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    public $prefix = '';
+
+    /**
+     * Database type
+     *
+     * @var \phpOMS\DataStorage\Database\DatabaseType
+     * @since 1.0.0
+     */
+    private $type = null;
+
+    /**
+     * Database status
+     *
+     * @var \phpOMS\DataStorage\Database\DatabaseStatus
+     * @since 1.0.0
+     */
+    public $status = \phpOMS\DataStorage\Database\DatabaseStatus::CLOSED;
+
+    /**
+     * Object constructor
+     *
+     * Creates the database object and overwrites all default values.
+     *
+     * @param string[] $dbdata the basic database information for establishing a connection
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function __construct($dbdata)
+    {
+        $this->type = \phpOMS\DataStorage\Database\DatabaseType::MYSQL;
+        $this->connect($dbdata);
+    }
+
+    /**
+     * Connect to database
+     *
+     * Overwrites current connection if existing
+     *
+     * @param string[] $dbdata the basic database information for establishing a connection
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function connect($dbdata) {
+        $this->close();
+
+        $this->dbdata = $dbdata;
+        $this->prefix = $dbdata['prefix'];
+
+        try {
+            $this->con = new \PDO($this->dbdata['db'] . ':host=' . $this->dbdata['host'] . ';dbname=' . $this->dbdata['database'] . ';charset=utf8', $this->dbdata['login'], $this->dbdata['password']);
+            $this->con->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+            $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+            $this->status = \phpOMS\DataStorage\Database\DatabaseStatus::OK;
+        } catch(\PDOException $e) {
+            $this->status = \phpOMS\DataStorage\Database\DatabaseStatus::MISSING_DATABASE;
+            $this->con    = null;
+        }
+    }
+
+    /**
+     * Get the database type
+     *
+     * @return \phpOMS\DataStorage\Database\DatabaseType
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Close database connection
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function close()
+    {
+        $this->con = null;
+        $this->status = \phpOMS\DataStorage\Database\DatabaseStatus::CLOSED;
+    }
+
+    /**
+     * Object destructor
+     *
+     * Sets the database connection to null
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function __destruct()
+    {
+        $this->close();
+    }
+}
