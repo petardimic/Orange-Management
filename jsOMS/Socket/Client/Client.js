b5d6281774ecdbc048a02d8e5410a6b4c1307634
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-05-23 10:15:29 +0200

    WebSocket sketch

---
+++ jsOMS/Socket/Client/Client.js
@@ -0,0 +1,48 @@
+(function (jsOMS, undefined) {
+    jsOMS.Client = function () {
+        this.port = 80;
+        this.ip = '127.0.0.1';
+        this.protocol = '';
+        this.connection = null;
+        this.messages = [];
+    };
+
+    jsOMS.Client.prototype.setMessage = function(id, callback) {
+        if(this.messages[id] === 'undefined') {
+            this.messages[id] = [];
+        }
+
+        this.messages.push()
+    };
+
+    jsOMS.Client.prototype.setIp = function(ip) {
+        this.ip = ip;
+    };
+
+    jsOMS.Client.prototype.setPort = function(port) {
+        this.port = port;
+    };
+
+    jsOMS.Client.prototype.setProtocol = function(protocol) {
+        this.protocol = protocol;
+    };
+
+    jsOMS.Client.prototype.connect = function() {
+        var self = this;
+        this.connection = new WebSocket(this.ip, this.protocol);
+
+        this.connection.onmessage = function(event) {
+            var msg = JSON.parse(event.data);
+
+            self.messages[msg.type](msg);
+        }
+    };
+
+    jsOMS.Client.prototype.send = function(msg) {
+        this.connection.send(JSON.stringify(msg));
+    };
+
+    jsOMS.Client.prototype.close = function() {
+        this.connection.close();
+    }
+}(window.jsOMS = window.jsOMS || {}));
