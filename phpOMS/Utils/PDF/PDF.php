1b8b5324bc51e7a5b868dd3f8b67b92a079cb38b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-05-30 23:43:22 +0200

    Renaming

--- phpOMS/Utils/PDF/PDF.php
+++ phpOMS/Utils/PDF/PDF.php
@@ -1,78 +1,9 @@
 <?php
 namespace phpOMS\Utils\Pdf;
 
-require_once __DIR__ . '\..\..\..\vendor\tecnick.com\tcpdf\tcpdf.php';
+require_once __DIR__ . '/../../../vendor/tcpdf/tcpdf.php';
 
-class PDF extends \TCPDF implements \phpOMS\Contract\RenderableInterface
+class PDF extends \TCPDF
 {
 
-// region Class Fields
-    private $pageNumberStart = 0;
-
-    private $pageNumberPages = [];
-
-    private $pageNumberStyle = null;
-
-    private $theme = null;
-
-// endregion
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    public function setTheme() {
-
-    }
-
-    public function setCreator()
-    {
-    }
-
-    public function setAuthor()
-    {
-    }
-
-    public function setOrientation()
-    {
-    }
-
-    public function setPageFormat()
-    {
-    }
-
-    public function setEncoding()
-    {
-    }
-
-    public function setMargin()
-    {
-    }
-
-    public function setHeaderMargin()
-    {
-    }
-
-    public function setFooterMargin()
-    {
-    }
-
-    public function setFont()
-    {
-    }
-
-    public function Header()
-    {
-        $this->theme->setHeader($this);
-    }
-
-    public function Footer()
-    {
-        $this->theme->setFooter($this);
-    }
-
-    public function render()
-    {
-    }
 }
